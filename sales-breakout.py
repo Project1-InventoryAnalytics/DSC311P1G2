61d41076bff2e5589c0a8b218deef29435df1ce4
lesterj2 <lesterj2@mymail.nku.edu>
2021-02-28 15:23:45 -0500

    Update sales-breakout.py

--- sales-breakout.py
+++ sales-breakout.py
@@ -1,13 +1,16 @@
 #import relevant modules
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import preprocessing as pp
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 
 desired_width = 600
 pd.set_option('display.width',desired_width)
-pd.set_option('display.max_columns',10)
+pd.set_option('display.max_columns',20)
 
+'''
 Data_Dir = "F:/NKU-DSC311 project files/"
 filename = "SalesFINAL12312016.csv"
 df = pd.read_csv(f"{Data_Dir}{filename}")
@@ -30,3 +33,22 @@ salesByStore.to_csv('TotalSalesByStore.csv')
 
 print(salesByStore)
 #TODO: use pivot or melt feature to create a report that inserts subtotal lines by store by year.
+'''
+
+def main():
+
+    # load data
+    df_2017_purchase_prices_0 = pd.read_csv("Datasets\\2017PurchasePricesDec.csv")
+    df_beg_inv_final_2016_0 = pd.read_csv("Datasets\\BegInvFINAL12312016.csv")
+    df_end_inv_final_2016_0 = pd.read_csv("Datasets\\EndInvFINAL12312016.csv")
+    df_invoice_purchases_2016_0 = pd.read_csv("Datasets\\InvoicePurchases12312016.csv")
+    df_purchases_final_2016_0 = pd.read_csv("Datasets\\PurchasesFINAL12312016.csv")
+    df_sales_final_2016_0 = pd.read_csv("Datasets\\SalesFINAL12312016.csv")
+
+    # clean data - address missing values, outliers, unrealistic values, ...
+
+    # prep/transform data for analysis - create new dfs, add colums to existing dfs, ...
+
+
+if __name__ == '__main__':
+    main()
